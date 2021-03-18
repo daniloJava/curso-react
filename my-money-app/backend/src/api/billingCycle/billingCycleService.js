2558706738e8ddfb3a1f61fe5d80400249c33fce
Danilo Manoel Oliveira da Silva <danilo.manoel@olxbr.com>
2021-03-18 01:52:29 -0300

    App my money backend

---
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,44 @@
+const BillingCycle = require('./billingCycles')
+const errorHandler = require('../commons/errorHandler')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
+
+BillingCycle.route('get', (req, res, next) => {
+    BillingCycle.find({}, (err, docs) => {
+        if (!err) {
+            res.json(docs)
+        } else {
+            res.status(500).json({ errors: [error] })
+        }
+    })
+})
+
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if (!error) {
+            res.json({ value })
+        } else {
+            res.status(500).json({ errors: [error] })
+        }
+    })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+    }, {
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+    }, {
+        $project: { _id: 0, credit: 1, debt: 1 }
+    }], (error, result) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            res.json(result[0] || { credit: 0, debt: 0 })
+        }
+    })
+})
+
+module.exports = BillingCycle
