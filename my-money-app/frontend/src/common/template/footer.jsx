1c8b4487a933f98a050deec9588ffb93782d379f
Danilo Manoel Oliveira da Silva <danilo.manoel@olxbr.com>
2021-06-03 16:33:35 -0300

    feature(dashboard) aplicando dashboard e componentes

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2021
+            <a href='https://www.linkedin.com/in/danilo-manoel-oliveira-da-silva/' target='_blank'> Danilo Manoel Oliveira da Silva</a>.
+        </strong>
+    </footer>
+)
---
+++ my-money-app/frontend/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import App from './main/app'
+import reducers from './main/reducers'
+import promise from 'redux-promise'
+
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+      && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers, devTools )
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app'))
