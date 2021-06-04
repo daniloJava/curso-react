092cb06f821d0ffb2b090ece976829f59428e777
Danilo Manoel Oliveira da Silva <danilo.manoel@olxbr.com>
2021-06-04 01:50:27 -0300

    feat(auth) aplicando autenticaçao

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,10 +1,24 @@
 const express = require('express')
-
-module.exports = function(server) {
-
-    const router = express.Router()
-    server.use('/api', router)
-
+const auth = require('./auth')
+module.exports = function (server) {
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    const AuthService = require('../api/user/AuthService')
+    
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
+    BillingCycle.register(protectedApi, '/billingCycles')
+    
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    
+    server.use('/oapi', openApi)
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
